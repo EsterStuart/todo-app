c7f58c209648cd45823382b8042d998b410f262a
LiaBebop <128011196+LiaBebop@users.noreply.github.com>
2024-11-25 11:37:39 -0500

    Baseline start for project, setting up todo-list file

---
+++ components/todo-list.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useState, useEffect, ChangeEvent, KeyboardEvent } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+
+interface Task {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+const [tasks, setTasks] = useState<Task[]>([]);
+const [newTask, setNewTask] = useState<string>("");
+const [editingTaskId, setEditingTaskId] = useState<number | null>(null);
+const [editedTaskText, setEditedTaskText] = useState<string>("");
+const [isMounted, setIsMounted] = useState<boolean>(false);
+
+useEffect(() => {
+    setIsMounted(true);
+    const savedTask = localStorage.getItem("tasks");
+    if (savedTask) {
+        setTasks(JSON.parse(savedTask) as Task[]);
+    }
+}, []);
+
